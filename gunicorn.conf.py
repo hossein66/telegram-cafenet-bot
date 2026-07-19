067fac13d7d1e3626c52fa9ff0bc3459112be614
Hossein Navabi <hossein.navabi@technohouse.com.tr>
2026-07-19 10:21:01 +0330

    fix api ports

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 # gunicorn.conf.py
-bind = "0.0.0.0:8000"  # or your preferred port
+bind = "0.0.0.0:8001"  # or your preferred port
 workers = 4
 worker_class = "uvicorn.workers.UvicornWorker"  # Since you're using FastAPI
 timeout = 120
